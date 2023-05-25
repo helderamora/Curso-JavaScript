8c03d3357130c12b6529be06a332398e842dfa3e
HAmora <helder_amora79@hotmail.com>
2023-05-25 19:24:26 -0300

    Operadores Aritiméticos

---
+++ operadores_aritimeticos.js
@@ -0,0 +1,24 @@
+// > OPERADORES ARITIMÉTICOS
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero;
+
+console.log('Soma =', soma);
+console.log('Subtração =', primeiroNumero - segundoNumero);
+console.log('Multiplicação =', primeiroNumero * segundoNumero);
+console.log('Divisão =', primeiroNumero / segundoNumero);
+console.log('Exponeciação =', primeiroNumero ** segundoNumero);
+console.log('Resto da divisão =', primeiroNumero % segundoNumero);
+
+// > PRECEDÊNCIA DE OPERADORES
+
+console.log(3 + 7 * 3);
+console.log((3 + 7) * 3);
+console.log((10 + 8 + 7) / 3);
+
+// > BÍBLIOTECA MATH
+
+console.log(Math.PI);
+console.log(Math.sqrt(16));
