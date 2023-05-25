85740fc57f7b79537de0c9301b2ec2c6b3a6c695
HAmora <helder_amora79@hotmail.com>
2023-05-25 18:53:33 -0300

    Interação entre HTML CSS JS

--- variaveis.js
+++ variaveis.js
@@ -1,5 +1,16 @@
 // VARIÁVEIS
 
-let idade = 26;
-idade = 30;
-console.log(idade);
+// let idade = 26;
+// idade = 30;
+// console.log(idade);
+
+function myFunction () {
+    let user = prompt("Diga seu nome");
+
+    if (user != null) {
+        document.getElementById('print').innerHTML = "Óla " + user + "!"
+
+    }
+}
+
+prompt(user);
