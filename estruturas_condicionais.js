37df7ced867739babda9aa05dcf5c0dd9a7fc60d
HAmora <helder_amora79@hotmail.com>
2023-05-25 23:12:36 -0300

    Estruturas condicionais

---
+++ estruturas_condicionais.js
@@ -0,0 +1,30 @@
+// > ESTRUTURAS CONDICIONAIS
+
+const idade = 10;
+
+if (idade >= 18) {
+    console.log('Você é maior de idade!');
+} else {
+    console.log('Você é menor de idade!');
+}
+
+/* 
+- Se media >= 7, Aprovado
+- Se madia < 7 e media > 5, Recuperação
+- Se media < 5, Reprovado
+*/
+
+console.clear(); // Comando para limpar código do terminal.
+
+
+let media = 4.99;
+
+if (media >= 7) {
+    console.log("Aprovado(a)");
+} else if (media >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado(a)");
+}
+
+
