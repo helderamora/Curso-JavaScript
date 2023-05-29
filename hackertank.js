861fd5b222e0313ae6a421977e80cb055ab86ec2
HAmora <helder_amora79@hotmail.com>
2023-05-29 06:48:47 -0300

    Desafio HackerRank

--- hackertank.js
+++ hackertank.js
@@ -3,8 +3,9 @@ const b = [3, 2, 1];
 
 // let countA = JSON.stringify(a);
 // let countB = JSON.stringify(b);
+ function readLine(a, b) {
 
-function readLine(a, b) {
-    
-}
- readLine(a[0] > b[0];)
+ }
+
+ console.log(`Array a: ${a}`);
+ console.log(`Array b: ${b}`);
